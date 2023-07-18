b19de443c2a8fda1d73eb5644a0d908a3e0322f6
HelenaGomezV <helena.villegas90@gmail.com>
2023-07-18 06:55:38 +0200

    variables y Constantes

--- src/main.go
+++ src/main.go
@@ -3,7 +3,32 @@ package main
 import "fmt"
 
 func main() {
+	// Declaracion de constantes
+	const pi float64 = 3.14
+	const pi2 = 3.14
 
-	fmt.Println("Hola mundo")
+	fmt.Println("pi", pi)
+	fmt.Println("pi2", pi2)
+
+	//Declaracion Variables
+	base := 12
+	var altura int = 14
+	var area int
+
+	fmt.Println(base, altura, area)
+
+	//zero values
+
+	var a int
+	var b float64
+	var c string
+	var d bool
+
+	fmt.Println(a, b, c, d)
+
+	//Area de un cuadrado
+	const baseCuadrado = 10
+	areaCuadrado := baseCuadrado * baseCuadrado
+	fmt.Println(areaCuadrado)
 
 }
