c53cf340f52e123c35ece4bc4d8ceb51a1e0f1a1
HelenaGomezV <helena.villegas90@gmail.com>
2023-07-18 16:50:57 +0200

    for in golang

--- src/main.go
+++ src/main.go
@@ -3,68 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	// Declaracion de constantes
-	const pi float64 = 3.14
-	const pi2 = 3.14
-
-	fmt.Println("pi", pi)
-	fmt.Println("pi2", pi2)
-
-	//Declaracion Variables
-	base := 12
-	var altura int = 14
-	var area int
-
-	fmt.Println(base, altura, area)
-
-	//zero values
-
-	var a int
-	var b float64
-	var c string
-	var d bool
-
-	fmt.Println(a, b, c, d)
-
-	//Area de un cuadrado
-	const baseCuadrado = 10
-	areaCuadrado := baseCuadrado * baseCuadrado
-	fmt.Println(areaCuadrado)
-
-	//operaciones artimeticas
-	// suma
-	x := 10
-	y := 50
-	result := x + y
-	fmt.Println("Suma:", result)
-
-	//resta
-	result = x - y
-	fmt.Println("Resta:", result)
-
-	//multiplicacion
-	result = x * y
-	fmt.Println("Multi:", result)
-
-	//Division
-	result = x / y
-	fmt.Println("Division:", result)
-
-	//modulo
-	result = x % y
-	fmt.Println("Modulo:", result)
-
-	//Incremental
-	result = x + 1
-	fmt.Println("Incremental:", result)
-
-	//Incremental
-	result = x - 1
-	fmt.Println("Decremental:", result)
-
-	// area de un rectangulo
-
-	result = base * altura / 2
-	fmt.Println("area Rectangulo:", result)
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+	}
+	counter := 0
+	for counter < 10 {
+		fmt.Println(counter)
+		counter++
+	}
 
 }
