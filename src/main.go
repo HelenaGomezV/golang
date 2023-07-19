1d057d288e9a385bc01178bf21ba15f047edfc8f
HelenaGomezV <helena.villegas90@gmail.com>
2023-07-19 11:40:27 +0200

    arrays son inmutables los slices son mutables

--- src/main.go
+++ src/main.go
@@ -3,32 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	//defer
-	defer fmt.Println("Hola")
-	fmt.Println("Mundo")
+	//arrays
+	var array [4]int
+	array[0] = 1
+	array[1] = 2
 
-	//buena practica para cerrar conecciones, son buenas practicas utilizar un solo difer por funcion
+	fmt.Println(array, len(array), cap(array))
 
-	// CONTINUE AND BREAK
+	slice := []int{0, 1, 2, 3, 4, 5, 6}
+	fmt.Println(slice, len(slice), cap(slice))
 
-	for i := 0; i < 10; i++ {
-		fmt.Println(i)
-		// continue
-		if i == 2 {
-			fmt.Println("Es 2")
-			continue
-		}
+	fmt.Println(slice[0])
+	fmt.Println(slice[:3])
+	fmt.Println(slice[2:4])
+	fmt.Println(slice[3:])
 
-	}
+	//append
 
-	for i := 0; i < 10; i++ {
-		fmt.Println(i)
-		// continue
-		if i == 8 {
-			fmt.Println("Es 8")
-			break
-		}
+	slice = append(slice, 7)
+	fmt.Println(slice, len(slice), cap(slice))
 
-	}
+	//append new list
+	newSlice := []int{8, 9, 10}
+	slice = append(slice, newSlice...)
+	fmt.Println(slice)
 
 }
