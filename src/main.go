797c40f5458b11733c63e9e99f95d9585fe96957
HelenaGomezV <helena.villegas90@gmail.com>
2023-07-19 12:25:56 +0200

    Palindrome exercise

--- src/main.go
+++ src/main.go
@@ -1,41 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
-func main() {
-	//arrays
-	var array [4]int
-	array[0] = 1
-	array[1] = 2
-
-	fmt.Println(array, len(array), cap(array))
-
-	slice := []int{0, 1, 2, 3, 4, 5, 6}
-	fmt.Println(slice, len(slice), cap(slice))
-
-	fmt.Println(slice[0])
-	fmt.Println(slice[:3])
-	fmt.Println(slice[2:4])
-	fmt.Println(slice[3:])
-
-	//append
-
-	slice = append(slice, 7)
-	fmt.Println(slice, len(slice), cap(slice))
-
-	//append new list
-	newSlice := []int{8, 9, 10}
-	slice = append(slice, newSlice...)
-	fmt.Println(slice)
-
-	// recorrer un array o un slice
-
-	slice2 := []string{"Vamos", "a ver", "que pasa"}
-
-	for i, valor := range slice2 {
-		fmt.Println(i, valor)
+func isPalindrome(text string) {
+	text2 := strings.ToLower(text)
+	textReverse := ""
+	for i := len(text2) - 1; i >= 0; i-- {
+		textReverse += string(text2[i])
+	}
+	if text2 == textReverse {
+		fmt.Println("is Palindrome")
+	} else {
+		fmt.Println("no es")
 	}
 
-	//
+}
+
+func main() {
+	isPalindrome("Ama")
 
 }
